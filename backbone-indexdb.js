1fe40824eb157d3b2da94f2a9f3d80cdd7a4a974
Anton Podviaznikov <podviaznikov@gmail.com>
2011-05-02 21:56:23 +0200

    a lot of updates. integration with m.player project as testing base.

--- backbone-indexdb.js
+++ backbone-indexdb.js
@@ -22,9 +22,53 @@ Porridge.Model = Backbone.Model.extend(
             this.set({id:this.id});
         }
     },
+    save:function()
+    {
+        var entityName = this.constructor.definition.name;
+        Porridge.save(entityName,this.toJSON(),this.id);
+    },
+    destroy:function()
+    {
+        var entityName = this.constructor.definition.name;
+        var keyName = this.constructor.definition.key||'id';
+        var model = this;
+        var success=function()
+        {
+            model.trigger('destroy', model, model.collection);
+        }
+        Porridge.remove(entityName,this.get(keyName),success);
+    }
+},
+{
     definition:{}
 });
 Porridge.Collection = Backbone.Collection.extend(
 {
-
+    fetch:function(options)
+    {
+        options || (options = {});
+        var collection = this;
+        var entityName = this.model.definition.name;
+        var addOne = function(data)
+        {
+            collection.add(new collection.model({attributes:data}));
+        }
+        Porridge.all(entityName,addOne,function()
+        {
+            collection.trigger('retrieved');
+        },options.error);
+    },
+    fetchByKey:function(keyName,keyValue)
+    {
+        var collection = this;
+        var entityName = this.model.definition.name;
+        var addOne = function(data)
+        {
+            collection.add(new collection.model({attributes:data}));
+        }
+        Porridge.allByKey(entityName,keyName,keyValue,addOne,function()
+        {
+            collection.trigger('retrieved');
+        });
+    }
 });
